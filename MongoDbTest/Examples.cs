df3f2198c2388f4008483ed5a2338e5526e7f335
Alin Banta <banta_alin@yahoo.com>
2018-10-03 16:43:58 +0300

    Added an example of how dynamically add a new field into a collection

---
+++ MongoDbTest/Examples.cs
@@ -0,0 +1,35 @@
+﻿using MongoDbTest.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDbTest
+{
+    public class Examples
+    {
+        private readonly UsersRepository _usersRepo;
+
+        public Examples()
+        {
+            _usersRepo = new UsersRepository("connString");
+        }
+
+        /// <summary>
+        /// Adds a new field in an existing document
+        /// Example: AddNewFieldInAnExistingDocument("Nikola","address", "test address");
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="fieldValue"></param>
+        private async void AddNewFieldInAnExistingDocument(string userName, string fieldName, string fieldValue)
+        {
+            var users = await _usersRepo.GetUsersByField("name", userName);
+
+            var user = users.FirstOrDefault();
+
+            await _usersRepo.UpdateUser(user.Id, fieldName, fieldValue);
+        }
+    }
+}
